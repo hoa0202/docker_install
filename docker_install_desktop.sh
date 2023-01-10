dbf8cb6d2d999ad0eee672a6581e566221c90939
hoa0202 <67028730+hoa0202@users.noreply.github.com>
2023-01-10 14:16:45 +0900

    Update docker_install_desktop.sh

--- docker_install_desktop.sh
+++ docker_install_desktop.sh
@@ -14,5 +14,17 @@ sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubun
 # Install docker
 sudo apt-get update && sudo apt-get install docker-ce docker-ce-cli containerd.io
 
+## Host
+
+sudo usermod -aG docker $USER
+
+
+distribution=$(. /etc/os-release;echo $ID$VERSION_ID)
+curl -s -L https://nvidia.github.io/nvidia-docker/gpgkey | sudo apt-key add -
+curl -s -L https://nvidia.github.io/nvidia-docker/$distribution/nvidia-docker.list | sudo tee /etc/apt/sources.list.d/nvidia-docker.list
+sudo apt-get update && sudo apt-get install -y nvidia-container-toolkit nvidia-docker2
+sudo systemctl restart docker
+
+
 # Check installation
 docker -v
