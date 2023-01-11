4e1a5376014240a37e55ce755d05774b624b5724
hoa0202 <67028730+hoa0202@users.noreply.github.com>
2023-01-11 10:41:26 +0900

    Update docker_install_desktop.sh

--- docker_install_desktop.sh
+++ docker_install_desktop.sh
@@ -3,7 +3,7 @@
 sudo apt-get update
 
 # Install dependencies
-sudo apt-get install apt-transport-https ca-certificates curl gnupg-agent software-properties-common
+sudo apt-get install -y apt-transport-https ca-certificates curl gnupg-agent software-properties-common
 
 # Add GPG key
 curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
@@ -12,7 +12,7 @@ curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
 sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu  $(lsb_release -cs) stable"
 
 # Install docker
-sudo apt-get update && sudo apt-get install docker-ce docker-ce-cli containerd.io
+sudo apt-get update -y  && sudo apt-get install -y docker-ce docker-ce-cli containerd.io
 
 ## Host
 
